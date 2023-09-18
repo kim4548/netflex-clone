8ee754162907cb1bdca2a5575bda0b70a65a773f
kim4548 <kdst6156@naver.com>
2023-09-18 15:05:23 +0900

    코드정리

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,14 +12,14 @@ function Banner() {
     fetchData();
   },[]);
 const fetchData =async () =>{
- //현재 상영중인 영화를 가져오기
+ 
    const  request = await axios.get(requests.fetchNowPlaying);
-   // 여러개 중의 영화 하나의 ID를 가져오기
+   
    const movieId = request.data.results[
     Math.floor(Math.random()*request.data.results.length)
    ].id
    
-   // 특정 영화의 더 상세한 정보를 가져오기 ( 비디오 정보도 포함)
+  
    const {data : movieDetail} = await axios.get(`movie/${movieId}`,{
     params:{append_to_response:"videos"},
    });
